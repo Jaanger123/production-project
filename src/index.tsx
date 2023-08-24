a44ad80ea43245c0b840823f2a0d2c3651ae1329
Jaanger123 <jbarakanov@inbox.ru>
2023-08-24 20:54:45 +0600

    Implementing code splitting

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,10 @@
-import { render } from 'react-dom';
-import App from './App';
+import { BrowserRouter } from "react-router-dom";
+import { render } from "react-dom";
+import App from "./App";
 
-render(<App />, document.getElementById('root'));
+render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>,
+    document.getElementById("root")
+);
