b62caf3623eb12ae6b9de419500ad6c02effbb46
Jaanger123 <jbarakanov@inbox.ru>
2023-09-05 21:17:47 +0600

    Creating first widget, configuring UI kit (AppLink)

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,19 +1,18 @@
-import { classNames } from "shared/lib/classNames/classNames";
-import { useTheme } from "./providers/ThemeProvider";
-import { Link } from "react-router-dom";
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useTheme } from './providers/ThemeProvider';
+import { AppRouter } from './providers/router';
+import { Navbar } from 'widgets/Navbar';
 
-import "./styles/index.scss";
-import { AppRouter } from "./providers/router";
+import './styles/index.scss';
 
 const App = () => {
     const { theme, toggleTheme } = useTheme();
 
     return (
-        <div className={classNames("app", {}, [theme])}>
-            <button onClick={toggleTheme}>TOGGLE</button>
-            <Link to={"/"}>Главная</Link>
-            <Link to={"/about"}>О сайте</Link>
+        <div className={classNames('app', {}, [theme])}>
+            <Navbar />
             <AppRouter />
+            <button onClick={toggleTheme}>TOGGLE</button>
         </div>
     );
 };
