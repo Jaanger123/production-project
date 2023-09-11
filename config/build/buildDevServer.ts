3f1d85472a788cf852fffb13aaab769d217a6389
Jaanger123 <jbarakanov@inbox.ru>
2023-09-11 19:42:55 +0600

    Adding react-refresh-webpack-plugin

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -1,10 +1,11 @@
-import { BuildOptions } from "./types/config";
-import type { Configuration as DevServerConfiguration } from "webpack-dev-server";
+import { BuildOptions } from './types/config';
+import type { Configuration as DevServerConfiguration } from 'webpack-dev-server';
 
 export function buildDevServer(options: BuildOptions): DevServerConfiguration {
     return {
         port: options.port,
         open: true,
         historyApiFallback: true, // This option is responsible for directing our request via index.html file, that allow us to reload page at any endpoint in browser (This error occures cause of SPA)
+        hot: true,
     };
 }
--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,13 +1,14 @@
+import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+import MiniExtractLoader from 'mini-css-extract-plugin';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import { BuildOptions } from './types/config';
 import webpack from 'webpack';
-import MiniExtractLoader from 'mini-css-extract-plugin';
 
 export function buildPlugins({
     paths,
     isDev,
 }: BuildOptions): webpack.WebpackPluginInstance[] {
-    return [
+    const plugins = [
         new HtmlWebpackPlugin({
             // Hepls us to inject minimized files to HTML file
             template: paths.html,
@@ -21,4 +22,10 @@ export function buildPlugins({
             __IS_DEV__: JSON.stringify(isDev),
         }),
     ];
+
+    if (isDev) {
+        plugins.push(new ReactRefreshWebpackPlugin()); // Display changes in browser without refreshing tab
+    }
+
+    return plugins;
 }
