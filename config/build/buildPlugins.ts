077bc8be0b907dd85a5482e7a37fb06ad4f7b6ee
Jaanger123 <jbarakanov@inbox.ru>
2023-12-18 19:19:34 +0600

    Adding webpack-bundle-analyzer

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,7 +1,9 @@
 import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import MiniExtractLoader from 'mini-css-extract-plugin';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import webpack from 'webpack';
+
 import { BuildOptions } from './types/config';
 
 export function buildPlugins({
@@ -20,6 +22,9 @@ export function buildPlugins({
         }),
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev),
+        }),
+        new BundleAnalyzerPlugin({
+            openAnalyzer: false,
         }),
     ];
 
